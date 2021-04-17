572ec579ae5d262368d8e9c3c0b566388aa9cbbf
でんろぼ <terablanc@outlook.jp>
2021-04-17 23:48:36 +0900

    [add] PBMライター追加
    動作確認はまだしていません

--- pnm/pbm.go
+++ pnm/pbm.go
@@ -10,6 +10,7 @@ var (
 	errBadPBMSample = errors.New("pnm: PBM画像のサンプル値が不正です")
 )
 
+// Reader用
 func (d *pnmDecoder) pbmReadRaster() (image.Image, error) {
 	var (
 		i, j, k int
@@ -39,4 +40,57 @@ func (d *pnmDecoder) pbmReadRaster() (image.Image, error) {
 		}
 	}
 	return img, nil
+}
+
+// Writer用
+func (e *pnmEncoder) pbmWriteRasterPlain(img image.Image) error {
+	var (
+		i, j int
+		y    uint32
+	)
+
+	rect := img.Bounds()
+	for i = rect.Min.Y; i < rect.Max.Y; i++ {
+		for j = rect.Min.X; j < rect.Max.X; j++ {
+			y, _, _, _ = rect.At(j, i).RGBA()
+			if y == 0 {
+				e.writer.WriteRune('0')
+			} else {
+				e.writer.WriteRune('1')
+			}
+			if j == rect.Max.X-1 {
+				e.writer.WriteRune('\n')
+			} else {
+				e.writer.WriteRune(' ')
+			}
+		}
+	}
+	return nil
+}
+func (e *pnmEncoder) pbmWriteRasterBinary(img image.Image) error {
+	var (
+		i, j, k int
+		y       uint32
+		b       byte
+	)
+
+	rect := img.Bounds()
+	for i = rect.Min.Y; i < rect.Max.Y; i++ {
+		for j = rect.Min.X; j < rect.Max.X; j += 8 {
+			b = 0x00
+			for k = 0; k < 8; k++ {
+				if (j + k) < rect.Max.X {
+					y, _, _, _ = rect.At(j+k, i).RGBA()
+				} else {
+					y = 0
+				}
+				b = b << 1
+				if y != 0 {
+					b |= 0x01
+				}
+			}
+			e.writer.WriteByte(b)
+		}
+	}
+	return nil
 }
--- pnm/writer.go
+++ pnm/writer.go
@@ -1,23 +1,124 @@
 package pnm
 
 import (
+	"bufio"
+	"errors"
+	"fmt"
 	"image"
 	"io"
 )
 
-func Encode(w io.Writer, img image.Image) error {
-	// ヘッダ情報の登録
+var (
+	errBadImageSize  = errors.New("pnm: 入力画像が不正です")
+	errUnsupportType = errors.New("pnm: サポートされていないModelです")
+)
 
-	switch img.(type) {
-	case *image.Gray, *image.Gray16:
-	case *image.NRGBA, *image.NRGBA64:
-	}
-	// ラスタデータの出力
-	return nil
+// グレースケールの場合はPGM形式で、カラー画像の場合はPPM形式でWriterに返す
+func Encode(w io.Writer, img image.Image) error {
+	var e pnmEncoder
+	return e.encode(w, img, "")
+}
+
+// magicに指定の形式でWriterに返す
+func EncodeWithType(w io.Writer, img image.Image, magic string) error {
+	var e pnmEncoder
+	return e.encode(w, img, magic)
 }
 
 type pnmHeader struct {
 	magicNumber   string
 	width, height int
 	maxValue      int
+}
+type pnmEncoder struct {
+	writer *bufio.Writer
+	h      pnmHeader
+}
+
+///
+// メソッド
+///
+func (e *pnmEncoder) encode(w io.Writer, img image.Image, magic string) error {
+	e.writer = bufio.NewWriter(w)
+	err := e.setHeader(img, magic)
+	if err != nil {
+		return err
+	}
+	switch sortPNM(e.h.magicNumber) {
+	case PBM:
+		fmt.Fprintf(e.writer,
+			"%s\n%d %d\n",
+			e.h.magicNumber,
+			e.h.width,
+			e.h.height,
+		)
+		if isPlain(e.h.magicNumber) {
+			return e.pbmWriteRasterPlain(img)
+		} else {
+			return e.pbmWriteRasterBinary(img)
+		}
+	case PGM:
+		fmt.Fprintf(e.writer,
+			"%s\n%d %d\n%d\n",
+			e.h.magicNumber,
+			e.h.width,
+			e.h.height,
+			e.h.maxValue,
+		)
+	case PPM:
+		fmt.Fprintf(e.writer,
+			"%s\n%d %d\n%d\n",
+			e.h.magicNumber,
+			e.h.width,
+			e.h.height,
+			e.h.maxValue,
+		)
+	}
+	return nil
+}
+
+func (e *pnmEncoder) setHeader(img image.Image, magic string) error {
+	// 画像サイズの取得
+	rect := img.Bounds()
+	if rect.Dx() <= 0 || rect.Dy() <= 0 {
+		return errBadImageSize
+	}
+	// ヘッダ情報の登録
+	e.h.width = rect.Dx()
+	e.h.height = rect.Dy()
+	switch img.(type) {
+	case *image.Gray, *image.NRGBA, *image.RGBA:
+		e.h.maxValue = 255
+	case *image.Gray16, *image.NRGBA64, *image.RGBA64:
+		e.h.maxValue = 65535
+	default:
+		return errUnsupportType
+	}
+	if magic == "" {
+		switch img.(type) {
+		case *image.Gray, *image.Gray16:
+			e.h.magicNumber = "P5"
+		case *image.NRGBA, *image.RGBA, *image.NRGBA64, *image.RGBA64:
+			e.h.magicNumber = "P6"
+		default:
+			return errUnsupportType
+		}
+	} else {
+		e.h.magicNumber = magic
+	}
+	return nil
+}
+
+//ラスター文字列がPlain形式のmagic numberか判別
+func isPlain(magic string) bool {
+	var (
+		i          int
+		plainMagic = [3]string{"P1", "P2", "P3"}
+	)
+	for i = 0; i < 3; i++ {
+		if magic == plainMagic[i] {
+			return true
+		}
+	}
+	return false
 }
